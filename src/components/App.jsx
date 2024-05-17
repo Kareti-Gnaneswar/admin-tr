a2dbc232586832614ebee7848406f7e8702a6088
kareti-gnaneswar <gnaneswarkareti@gmail.com>
2024-05-17 07:03:27 +0530

    add MCQComponent

---
+++ src/components/App.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import './App.css';
+import HRQuestions from './hrQuestions';
+import MCQComponent from './MCQs';
+
+function App() {
+  const [selectedButton, setSelectedButton] = useState(null);
+  
+  const options = [
+    "Send a Notification",
+    "Create Subject",
+    "TR questions",
+    "HR questions",
+    "MCQs",
+    "Mock Interview Requests",
+    "Mentoring Requests",
+    "Logout"
+  ];
+
+  const handleButtonClick = (buttonText) => {
+    setSelectedButton(buttonText);
+  };
+
+  const renderContent = () => {
+    switch (selectedButton) {
+      case 'Send a Notification':
+        return <div>Send a Notification content</div>;
+      case 'Create Subject':
+        return <div>Create Subject</div>;
+      case 'TR questions':
+        return <div>Upload TR questions</div>;
+      case 'HR questions':
+        return <HRQuestions />;
+      case 'MCQs':
+        return <MCQComponent/>;
+      case 'Mock Interview Requests':
+        return <div>Mock Interview Requests</div>;
+      case 'Mentoring Requests':
+        return <div>Mentoring Requests</div>;
+      case 'Logout':
+        return <div>Logout content</div>;
+      default:
+        return <div>Select an option</div>;
+    }
+  };
+
+  return (
+    <div className="container">
+      <div className="side">
+        <ul>
+          {options.map(option => (
+            <button key={option} onClick={() => handleButtonClick(option)}>
+              <li>{option}</li>
+            </button>
+          ))}
+        </ul>
+      </div>
+      <div className="content">
+        {renderContent()}
+      </div>
+    </div>
+  );
+}
+
+export default App;
